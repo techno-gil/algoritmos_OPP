70ddaae57706f400ec504c0b43a9967ad34bf0f5
techno-gil <gilsongonzales@hotmail.com>
2026-01-18 22:30:43 +0900

    exemplo 01

---
+++ aula_algoritmo.py
@@ -0,0 +1,11 @@
+print('='*40)
+nome = input('DIgite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+ano_atual = 2026
+
+ano_nascimento = ano_atual - idade
+
+print('-'*40)
+print(f'{nome} você nasceu em: {ano_nascimento}')
+print('='*40)

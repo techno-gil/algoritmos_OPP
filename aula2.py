e9b9ac208626ef2dff989c64c0064c0524cb9132
techno-gil <gilsongonzales@hotmail.com>
2026-01-18 23:14:12 +0900

    exemplo 02

---
+++ aula2.py
@@ -0,0 +1,12 @@
+print('='*40)
+numero = int(input('Digite um número: '))
+print('-'*40)
+
+if (numero < 0):
+    print('Negativo')
+elif (numero == 0):
+    print('Zero')
+else:
+    print('Positivo')
+print('-'*40)
+
